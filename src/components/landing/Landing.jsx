0eb27fceb21dd0deb12c757db005bf0ded6659dd
Franco Guevara <francoguevara93@gmail.com>
2024-03-06 16:19:25 -0300

    (style) New alerts styles

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,5 +1,7 @@
-import { Box, Container } from "@mui/material";
 import "./Landing.css";
+
+import { Box, Container } from "@mui/material";
+
 import GameSection from "./GameSection/GameSection";
 import RankingSection from "./RankingSection/RankingSection";
 import WeeklyOffers from "./WeeklyOffers/WeeklyOffers";
@@ -7,7 +9,7 @@ import dividerImg from "../../assets/images/backgrounds/divider.png";
 import SupportSection from "./SupportSection/SupportSection";
 
 const Landing = () => {
- 
+
   return (
     <>
       <Container maxWidth={false} disableGutters>
@@ -19,15 +21,15 @@ const Landing = () => {
         
         <GameSection/>
         <Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}}>
-          <img src={dividerImg} alt="" className="divider-image"/>
+          <img src={dividerImg} alt="Divider image" className="divider-image"/>
         </Box>
         <RankingSection/>
         <Box sx={{width: '100%', display: 'flex', justifyContent: 'center', mt: 10}}>
-          <img src={dividerImg} alt="" className="divider-image"/>
+          <img src={dividerImg} alt="Divider image" className="divider-image"/>
         </Box>
         <WeeklyOffers/>
         <Box sx={{width: '100%', display: 'flex', justifyContent: 'center', mt: 10}}>
-          <img src={dividerImg} alt="" className="divider-image"/>
+          <img src={dividerImg} alt="Divider image" className="divider-image"/>
         </Box>
         <SupportSection/>
       </Container>
--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -1,62 +1,63 @@
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 export const customAlert = (title, text, icon, action = null) => {
-    Swal.fire({
-        title,
-        text,
-        icon,
-        showCancelButton: true,
-        confirmButtonColor: '#3085D6',
-        cancelButtonColor: '#d33',
-        cancelButtonText: 'Cancelar',
-        confirmButtonText: 'Aceptar',
-        customClass: {
-            container: 'alert-container',
-        },
-    }).then((result) => {
-        if (result.isConfirmed) {
-            if (typeof action === 'function') {
-                action();
-            }
-        }
-    });
+  Swal.fire({
+    title,
+    text,
+    icon,
+    showCancelButton: true,
+    confirmButtonColor: "#3085D6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Aceptar",
+    customClass: {
+      container: "alert-container",
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if (typeof action === "function") {
+        action();
+      }
+    }
+  });
 };
 
-export const autoCloseAlert = (title, icon, background) => {
-    Swal.fire({
-        position: "bottom",
-        title,
-        icon,
-        background,
-        color: "#fff",
-        showConfirmButton: false,
-        toast: true,
-        timer: 2000,
-        customClass: {
-            container: 'alert-container',
-        },
-    });
-}
+export const autoCloseAlert = (title, type) => {
+  Swal.fire({
+    position: "bottom",
+    title: `<div class="text-modal-container"><span class="text-${type}-modal">${title}</span></div>`,
+    color: "black",
+    showConfirmButton: false,
+    toast: true,
+    width: 300,
+    timer: 2000,
+    background: "transparent",
+    customClass: {
+      container: "alert-container",
+      popup: type,
+    },
+  });
+};
 
 export const autoCloseAlertWithImage = (title, image, width, height) => {
-    Swal.fire({
-        html: `<img src=${image} style="display: block; margin: auto; width: ${width}px; height: ${height}px;" />`,
-        title: `<p style="text-shadow: 5px 5px 5px black; color: red; font-weight: bolder">${title}</p>`,
-        showConfirmButton: false,
-        timer: 1500,
-        position: 'center',
-        customClass: {
-            container: 'alert-container',
-            popup: 'alert-popup',
-        },
-        background: 'transparent',
-        backdrop: false,
-        allowOutsideClick: false,
-        showClass: {
-            popup: 'animate__animated animate__zoomIn'
-        },
-        hideClass: {
-            popup: 'animate__animated animate__zoomOut'
-        }
-    });
-}
+  Swal.fire({
+    html: `<img src=${image} style="display: block; margin: auto; width: ${width}px; height: ${height}px;" />`,
+    title: `<p style="text-shadow: 5px 5px 5px black; color: red; font-weight: bolder">${title}</p>`,
+    showConfirmButton: false,
+    timer: 1500,
+    position: "bottom",
+    customClass: {
+      container: "alert-container",
+      popup: "alert-popup",
+    },
+    background: "transparent",
+    backdrop: false,
+    allowOutsideClick: false,
+    showClass: {
+      popup: "animate__animated animate__zoomIn",
+    },
+    hideClass: {
+      popup: "animate__animated animate__zoomOut",
+    },
+  });
+};
