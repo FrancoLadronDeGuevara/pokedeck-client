539bf7c839b70fe998d2ce7d970b66af19073c98
Franco Guevara <francoguevara93@gmail.com>
2024-03-06 17:15:53 -0300

    (feature) Add route for user sending email support

--- src/components/landing/SupportSection/SupportSection.jsx
+++ src/components/landing/SupportSection/SupportSection.jsx
@@ -1,13 +1,15 @@
-import { Box, Container, TextField, Typography } from "@mui/material";
 import "./SupportSection.css";
-import { useState } from "react";
-import axios from "axios";
-import { autoCloseAlert } from "../../../utils/alerts";
-import { useSelector } from "react-redux";
-import Loader from "../../loader/Loader";
 
-const subjectRegex = /^[a-zA-Z0-9 ]{4,60}$/;
-const messageRegex = /^[a-zA-Z0-9 ]{10,255}$/;
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Box, Container, TextField, Typography } from "@mui/material";
+
+import { autoCloseAlert } from "../../../utils/alerts";
+import Loader from "../../loader/Loader";
+import clientAxios from "../../../utils/clientAxios";
+
+const subjectRegex = /^(?=.*\S)[\s\S]{4,60}$/;
+const messageRegex = /^(?=.*\S)[\s\S]{4,255}$/;
 
 const SupportSection = () => {
   const { user } = useSelector((state) => state.user);
@@ -20,47 +22,46 @@ const SupportSection = () => {
     setLoading(true);
     if (!message || !subject) {
       setLoading(false);
-      return autoCloseAlert(
-        "Completa el formulario correctamente",
-        "error",
-        "red"
-      );
+      return autoCloseAlert("¡Completa el formulario correctamente!", "error");
     }
 
     if (!subjectRegex.test(subject)) {
       setLoading(false);
       return autoCloseAlert(
-        "El asunto debe tener de 4 a 60 caracteres",
-        "error",
-        "red"
+        "¡El asunto debe tener de 4 a 60 caracteres!",
+        "error"
       );
     }
     if (!messageRegex.test(message)) {
       setLoading(false);
       return autoCloseAlert(
-        "El mensaje debe tener de 10 a 255 caracteres",
-        "error",
-        "red"
+        "¡El mensaje debe tener de 4 a 255 caracteres!",
+        "error"
       );
     }
 
     if (user) {
-      await axios.post("https://formsubmit.co/ajax/francoguevara93@gmail.com", {
-        subject,
-        message,
-        email: user.email,
-      });
-
-      autoCloseAlert("Gracias por tu mensaje", "success", "green");
-      setMessage("");
-      setSubject("");
-      setLoading(false);
+      await clientAxios
+        .post(`/users/send-support-email`, {
+          subject,
+          message,
+          email: user.email,
+        })
+        .then((res) => {
+          setLoading(false);
+          autoCloseAlert(res.data.message, "success");
+          setMessage("");
+          setSubject("");
+        })
+        .catch((error) => {
+          setLoading(false);
+          autoCloseAlert(error, "error");
+        });
     } else {
       setLoading(false);
       autoCloseAlert(
-        "Debes iniciar sesión para enviar el formulario",
-        "error",
-        "red"
+        "¡Debes iniciar sesión para enviar el formulario!",
+        "error"
       );
     }
   };
